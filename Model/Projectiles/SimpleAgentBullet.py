16d3ac9a6be1a932ea88c284b2d275ecdfa36e39
radil <adil.r@northeastern.edu>
2022-11-17 15:21:16 -0500

    Refactoring SimpleBulletAgent

--- Model/Projectiles/SimpleAgentBullet.py
+++ Model/Projectiles/SimpleAgentBullet.py
@@ -1,42 +1,57 @@
 import uuid
 
+from Model.Agents.Actions import Actions
+from Model.Agents.AgentInterface import AgentInterface
 from Model.Agents.Directions import Directions
+from Model.Agents.PlayerAgent import PlayerAgent
 from Model.Projectiles.ProjectileSuperClass import ProjectileSuperClass
 
 
 class SimpleAgentBullet(ProjectileSuperClass):
     
-    def __init__(self, currentAgent, speed = 1):
-        
-        
-        super().__init__(length=1, height=1, lowest_row=currentAgent.lowest_row, least_col=0, hp=1,
-                speed = 1, direction = Directions.LEFT)
-      
-        self.statPos(currentAgent)
-        self.currentAgent = currentAgent
-        self.direction = self.startDir(currentAgent)
-        self.id = uuid.uuid4()
+    def __init__(self, currentAgent: AgentInterface, speed = 1):
+
+        action_to_take = None
+
+        #default motion for simple bullet
+        if currentAgent.isPlayer():
+            action_to_take = Actions.RIGHT
+            self.agent_is_player_flag = True
+        else:
+            action_to_take = Actions.LEFT
+            self.agent_is_player_flag = False
+
+        self.direction = action_to_take
+
+        super().__init__(action_to_take, projectile_length=1, projectile_height=1, hp=1, speed = speed)
+
+        reference_agent_pos = currentAgent.get_position()
+        self.lowest_row = reference_agent_pos[0]
+
+        #set position of bullet
+        if self.agent_is_player_flag:
+            #must be placed one col ahead of player agent
+            self.least_col = reference_agent_pos[1] + 1
+        else:
+            self.least_col = reference_agent_pos[1] - 1
+
+
+    def isPlayer(self) -> bool:
+        #is a bullet this method does not apply
+        raise RuntimeError('A bullet is not a player or enemy agent, cannot call isPlayer on a ProjectileSuperClass type object')
+
 
     def isPlayerBullet(self):
-        return self.currentAgent.isPlayer()
-    
-    def startDir(self, currentAgent):
-        if currentAgent.isPlayer():
-            self.direction = Directions.RIGHT
-        else:
-            self.direction = Directions.LEFT
-    
-    def startPos(self, currentAgent):
-        if currentAgent.isPlayer():
-            self.least_col = currentAgent.get_max_col_boundary + 1
-        else:
-            self.least_col = currentAgent.get_min_col_boundary - 1
+        return self.agent_is_player_flag
+
 
     def copy(self):
-        copy = SimpleAgentBullet(self.lowest_row, self.least_col, self.currentAgent, self.direction)
-        copy.hasAlreadyMoved = self.hasAlreadyMoved
-        copy.id = self.id
-        return copy
+        #agent placeholder, need it since constructor relies on an AgentInterface
+        # will override values to match original SimpleBulletAgent
+        placeholder = PlayerAgent()
+        copy = SimpleAgentBullet(placeholder,self.speed)
+        copy.least_col = self.least_col
+        copy.lowest_row = self.lowest_row
+        copy.agent_is_player_flag = self.agent_is_player_flag
+        copy.direction = self.direction
 
-    def getId(self):
-        return self.id
