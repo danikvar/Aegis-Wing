c64a2f45c7597f25e4f7636e9de5652fb5964cac
radil <adil.r@northeastern.edu>
2022-11-26 11:28:39 -0500

    Ramzi added enemy spawn class

--- Model/EnemyPicker.py
+++ Model/EnemyPicker.py
@@ -3,17 +3,31 @@ import random
 from Model.Agents.AgentInterface import AgentInterface
 
 #TODO write tests
-class EnemyPicker():
+from Model.GameBoard import GameBoard
 
-    def __init__(self):
+
+class EnemyPicker():
+    #TODO test
+    def __init__(self, gameboard: GameBoard, spawn_rate: int):
         """
         Constructor for the EnemySpawnBehaviorSuperClass
         """
         #form associative array
-        self.enemy_spawn_list = []
-        self.enemy_weights = []
+        self.enemy_spawn_list = [] #list of AgentInterface Type objects
+        self.enemy_weights = [] # list of ints of weights being chosen
         self.initialized = False
+        self.board_min_col, self.board_max_col, \
+        self.board_min_row, self.board_max_row = gameboard.getBoardBoundaries()
 
+        #This logic should be in gameloop
+        # if spawn_rate < 0:
+        #     raise ValueError("Spawn rate cannot be less than 0 (0%), must be 0 >= spawn_rate <= 100 ")
+        # if spawn_rate > 100:
+        #     raise ValueError("Spawn rate cannot be greater than 100 (100%), must be  0 >= spawn_rate <= 100  ")
+
+        self.spawn_rate = spawn_rate
+
+    #TODO test
     def add_enemy_to_spawn_list(self, enemy: AgentInterface, weight: int):
         """
         Adds an enemy to the spawn list with a weight. The higher
@@ -40,6 +54,9 @@ class EnemyPicker():
         """
         # check probabilities len matches spawn list len because they are
         #supposed to be associative array
+        if (len(self.enemy_spawn_list) == 0):
+            print("Warning no agents added to enemy spawn list")
+
         if len(self.enemy_spawn_list) != len(self.enemy_weights):
             raise RuntimeError("ERROR: enemy spawn list and probability mismatch\nPlease check len of spawn list and len weights are the same")
 
@@ -54,6 +71,28 @@ class EnemyPicker():
         """
         if self.initialized == False:
             raise RuntimeError("Spawn Behavior not initialized, please call .initialize_spawn_behavior method")
-        random.choices(population=self.enemy_spawn_list,
+
+        #choose enemy by weight
+        chosen_enemy: AgentInterface = random.choices(population=self.enemy_spawn_list,
                        weights=self.enemy_weights,
                        k=1)
+
+        valid_pos = False
+
+        while valid_pos == False:
+            agent_lowest_row = random.randint(self.board_min_row, self.board_max_row)
+            chosen_enemy.set_position(agent_lowest_row, self.board_max_col)
+            if self.isValidEnemyPosition(chosen_enemy):
+                valid_pos = True
+
+        return chosen_enemy
+
+    def isValidEnemyPosition(self, agent : AgentInterface):
+        agent_row_min = agent.get_min_row_boundary()
+        agent_row_max = agent.get_max_row_boundary()
+
+        if (agent_row_min >= self.board_min_row) and (agent_row_max <= self.board_max_row):
+            return True
+        else:
+            return False
+
